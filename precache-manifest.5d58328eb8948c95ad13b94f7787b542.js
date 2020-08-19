809c17481659fd925333fb5f606fc67b473e8691
Gabriella <g.westberg@hotmail.se>
2020-08-19 09:43:41 +0200

    Updates

---
+++ precache-manifest.5d58328eb8948c95ad13b94f7787b542.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae01aa3970ece55abe13270eaf95fcb6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cd9386b4a864b846bf95",
+    "url": "/portfolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "01e9ddb5d4acd06a9b28",
+    "url": "/portfolio/static/css/main.d39b9c80.chunk.css"
+  },
+  {
+    "revision": "cd9386b4a864b846bf95",
+    "url": "/portfolio/static/js/2.c54683da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.c54683da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e9ddb5d4acd06a9b28",
+    "url": "/portfolio/static/js/main.23d940b7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "83d49e76bf05ee44052fecc95cb6357d",
+    "url": "/portfolio/static/media/realprofilepic.83d49e76.jpg"
+  }
+]);
