964fdb45da34adb29b5bc1f880abef3b57371d7e
Gabriella <g.westberg@hotmail.se>
2020-08-19 13:00:17 +0200

    Updates

--- precache-manifest.e4c73ae894201aeffad616e746cb569f.js
+++ precache-manifest.e4c73ae894201aeffad616e746cb569f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0d5efffdcdf3fa33bc6d09f45a4bbbfa",
+    "revision": "784a54ae7860332d48d346e0a7419eb9",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.f3cffc9e.chunk.css"
   },
   {
-    "revision": "bb9849a3358b14dba1b3",
-    "url": "/portfolio/static/css/main.84805259.chunk.css"
+    "revision": "7ae1c11e9ffafc25370e",
+    "url": "/portfolio/static/css/main.ca08e2d1.chunk.css"
   },
   {
     "revision": "e3ddc94b816a8b278964",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.cea132ef.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bb9849a3358b14dba1b3",
-    "url": "/portfolio/static/js/main.8b0b4d58.chunk.js"
+    "revision": "7ae1c11e9ffafc25370e",
+    "url": "/portfolio/static/js/main.68c9365d.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0f6e1b1e0a4b0912f0a0133d20e7ef16.js"
+  "/portfolio/precache-manifest.e4c73ae894201aeffad616e746cb569f.js"
 );
 
 self.addEventListener('message', (event) => {
