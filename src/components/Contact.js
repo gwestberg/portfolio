be6c8162b704a6192d52b6ba5b0aab89147d26ee
Gabriella <g.westberg@hotmail.se>
2020-08-19 09:51:29 +0200

    deploy

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Modal} from 'react-bootstrap';
 
 export default class ModalExample extends Component {
   constructor(props) {
@@ -32,32 +31,33 @@ export default class ModalExample extends Component {
 // </li>
   render() {
     return (
-      <Modal {...this.props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            <p>Contact Me</p>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="container">
-            <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-              <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input type="text" className="form-control" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" aria-describedby="emailHelp" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="message">Message</label>
-                <textarea className="form-control" rows="5"></textarea>
-              </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-          </div>
-        </Modal.Body>
-      </Modal>
+      <div></div>
+      // <Modal {...this.props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+      //   <Modal.Header closeButton>
+      //     <Modal.Title id="contained-modal-title-vcenter">
+      //       <p>Contact Me</p>
+      //     </Modal.Title>
+      //   </Modal.Header>
+      //   <Modal.Body>
+      //     <div className="container">
+      //       <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+      //         <div className="form-group">
+      //           <label htmlFor="name">Name</label>
+      //           <input type="text" className="form-control" />
+      //         </div>
+      //         <div className="form-group">
+      //           <label htmlFor="exampleInputEmail1">Email address</label>
+      //           <input type="email" className="form-control" aria-describedby="emailHelp" />
+      //         </div>
+      //         <div className="form-group">
+      //           <label htmlFor="message">Message</label>
+      //           <textarea className="form-control" rows="5"></textarea>
+      //         </div>
+      //         <button type="submit" className="btn btn-primary">Submit</button>
+      //       </form>
+      //     </div>
+      //   </Modal.Body>
+      // </Modal>
 
     );
   }
