f26b3644194b63c7aae9f17ff43ef77800485687
Gabriella <g.westberg@hotmail.se>
2020-08-20 09:00:27 +0200

    dynamic images working!!!!

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import Blogposts from './json/blogposts.json'
 
 export default class Blog extends Component {
     render() {
         return (
-            <div>some content</div>
+            <div>
+                {/* // <!-- Page Content --> */}
+                <div className="container">
+                    <div className="column">
+                    {
+                        Object.keys(Blogposts).map(key => (
+                            <div className="" key={key}>
+                                <div className=" h-100">
+                                    {/* <a href={Blogposts[key].link}></a> */}
+                                    <div className="card-body" >
+                                        <h4 className="card-title">{Blogposts[key].title}</h4>
+                                        <h6>- {Blogposts[key].date}</h6>
+                                        <p className="card-text">{Blogposts[key].text}</p>
+                                        <h6>tags:</h6>
+                                        <span>{Blogposts[key].tags}</span>
+                                    </div>
+                                </div>
+                        </div>
+                        ))
+                    }
+                        {/* <!-- /.row --> */}
+                    </div>
+                    {/* <!-- /.container --> */}
+                </div>
+            </div>
         )
     }
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './css/Projects.css'
-import Projectfiles from './json/projects.json'
+import './css/Projects.css';
+import Projectfiles from './json/projects.json';
 
 
 export default class Projects extends Component {
@@ -11,14 +11,7 @@ export default class Projects extends Component {
     }
     render() {
         return (
-            
             <div>
-            <div>
-                    <ol>
-                    
-                    </ol>
-                </div>
-                {/* // <!-- Page Content --> */}
                 <div className="container">
                     <div className="row row-wrap">
                     {
@@ -26,21 +19,18 @@ export default class Projects extends Component {
                             <div className="col-lg-4 col-sm-6 mb-4" key={key}>
                                 <div className=" h-100">
                                     <a href={Projectfiles[key].link}>
-                                    <img className="card-img-top" src={Projectfiles[key].image} alt="project thumbnail" /></a>
+                                    <img className="card-img-top" src= {require("../assets/"+Projectfiles[key].image)} alt="project thumbnail" />
+                                    </a>
                                     <div className="card-body" >
                                         <h4 className="card-title">{Projectfiles[key].name}</h4>
                                         <h6>- {Projectfiles[key].date}</h6>
                                         <p className="card-text">{Projectfiles[key].description}</p>
-                                        <h6>tags:</h6>
-                                        <span>{Projectfiles[key].tags}</span>
                                     </div>
                                 </div>
                         </div>
                         ))
                     }
-                        {/* <!-- /.row --> */}
                     </div>
-                    {/* <!-- /.container --> */}
                 </div>
             </div>
         )
