56e33d87e6f6773826f720a1184c89aeecc8eac4
Gabriella <g.westberg@hotmail.se>
2020-08-21 12:10:17 +0200

    Footer styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Link, animateScroll as scroll } from "react-scroll";
 import './css/Home.css'
@@ -35,26 +35,38 @@ export default class Home extends Component {
                     <nav className="navbar navbar-expand-sm navbar-dark transparent-nav">
                         <div className="container">
                             <a className="navbar-brand" href="/portfolio">Home</a>
-                            <button onClick={this.toggleNavbar} 
-                                    className={`${classTwo}`} type="button" 
-                                    data-toggle="collapse" 
-                                    data-target="#navbarResponsive" 
-                                    aria-controls="navbarResponsive" 
-                                    aria-expanded="false" 
-                                    aria-label="Toggle navigation"
-                                >
+                            <button onClick={this.toggleNavbar}
+                                className={`${classTwo}`} type="button"
+                                data-toggle="collapse"
+                                data-target="#navbarResponsive"
+                                aria-controls="navbarResponsive"
+                                aria-expanded="false"
+                                aria-label="Toggle navigation"
+                            >
                                 <span className="navbar-toggler-icon" />
                             </button>
                             <div className={`${classOne}`} id="navbarResponsive">
                                 <ul className="navbar-nav ">
-                                <li className="nav-item">
-                                <Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500}>
-                                        <a className="nav-link">About</a>
+                                    <li className="nav-item">
+                                        <Link
+                                            activeClass="active"
+                                            to="about"
+                                            spy={true}
+                                            smooth={true}
+                                            offset={-70}
+                                            duration={500}>
+                                            <a className="nav-link">About</a>
                                         </Link>
                                     </li>
                                     <li className="nav-item">
-                                    <Link activeClass="active" to="projects" spy={true} smooth={true} offset={-70} duration={500}>
-                                        <a className="nav-link">Projects</a>
+                                        <Link
+                                            activeClass="active"
+                                            to="projects"
+                                            spy={true}
+                                            smooth={true}
+                                            offset={-70}
+                                            duration={500}>
+                                            <a className="nav-link">Projects</a>
                                         </Link>
                                     </li>
                                     {/* <li className="nav-item">
@@ -70,22 +82,37 @@ export default class Home extends Component {
                 {/* HEADER */}
                 <section id="Header">
                     <Fade left cascade>
-                    <div id="headerContent">
-                        <h1 className="header-title">Hi, my name is Gabriella!</h1>
-                        <p>I'm a software developer student @<a className="link" href="https://www.boras.se/utbildningochforskola/borasyrkeshogskola/utbildningar/systemutvecklarenet.4.3fde8c191684442015123edf.html" target="_blank" rel="noopener noreferrer">YHBORÅS</a></p>
-                        <ul className="nav nav-pills flex-row">
-                            <li className="nav-item">
-                                <a className="nav-link" href="https://github.com/gwestberg" target="_blank" rel="noopener noreferrer"><FaGithub size={50} /></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="https://www.linkedin.com/in/gabriella-w-54975ba7/" target="_blank" rel="noopener noreferrer"><FaLinkedin size={50} /></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="mailto:g.westberg@hotmail.se" target="_blank" rel="noopener noreferrer"><AiOutlineMail size={50} /></a>
-                            </li>
-                        </ul>
-                    </div>
-                        </Fade>
+                        <div id="headerContent">
+                            <h1 className="header-title">Hi, my name is Gabriella!</h1>
+                            <p>I'm a software developer student @<a className="link" href="https://www.boras.se/utbildningochforskola/borasyrkeshogskola/utbildningar/systemutvecklarenet.4.3fde8c191684442015123edf.html" target="_blank" rel="noopener noreferrer">YHBORÅS</a></p>
+                            <ul className="nav nav-pills flex-row">
+                                <li className="nav-item">
+                                    <a className="nav-link"
+                                        href="https://github.com/gwestberg"
+                                        target="_blank"
+                                        rel="noopener noreferrer">
+                                        <FaGithub size={50} />
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link"
+                                        href="https://www.linkedin.com/in/gabriella-w-54975ba7/"
+                                        target="_blank"
+                                        rel="noopener noreferrer">
+                                        <FaLinkedin size={50} />
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link"
+                                        href="mailto:g.westberg@hotmail.se"
+                                        target="_blank"
+                                        rel="noopener noreferrer">
+                                        <AiOutlineMail size={50} />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </Fade>
                 </section>
                 {/* ------------ */}
 
@@ -100,9 +127,9 @@ export default class Home extends Component {
 
                         {/* PROJECTS */}
                         <section id="projects">
-                            
-                                <Projects></Projects>
-                                <br />
+
+                            <Projects></Projects>
+                            <br />
                         </section>
                         {/* ------------ */}
 
@@ -121,21 +148,38 @@ export default class Home extends Component {
 
                 {/* FOOTER */}
                 <footer className="page-footer font-small ">
-                <h3 className="footer-title">Contact me:</h3>
-                <ul className="nav nav-pills flex-row justify-content-center">
-                            <li className="nav-item">
-                                <a className="nav-link" href="https://github.com/gwestberg" target="_blank" rel="noopener noreferrer"><FaGithub size={40} /></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="https://www.linkedin.com/in/gabriella-w-54975ba7/" target="_blank" rel="noopener noreferrer"><FaLinkedin size={40} /></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="mailto:g.westberg@hotmail.se" target="_blank" rel="noopener noreferrer"><AiOutlineMail size={40} /></a>
-                            </li>
-                        </ul>
+                    <h3 className="footer-title">Contact me:</h3>
+                    <ul className="nav nav-pills flex-row justify-content-center">
+                        <li className="nav-item">
+                            <a className="nav-link"
+                                href="https://github.com/gwestberg"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                <FaGithub size={40} />
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link"
+                                href="https://www.linkedin.com/in/gabriella-w-54975ba7/"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                <FaLinkedin size={40} />
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link"
+                                href="mailto:g.westberg@hotmail.se"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                <AiOutlineMail size={40} />
+                            </a>
+                        </li>
+                    </ul>
                     {/* <!-- Copyright --> */}
-                    <div className="footer-copyright text-center text-light py-3">© 2020 Copyright:
-                        <a href="http://localhost:3000/#"> Pistorella</a>
+                    <div className="footer-copyright text-center text-light py-3">
+                        © 2020 Copyright:
+                        <a className="footer-link" href="/portfolio"> Pistorella</a>
+                    <p className="footer-text-small">Built with <a className="footer-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">react.js</a> </p>
                     </div>
                     {/* <!-- Copyright --> */}
                 </footer>
